a9dacf71c9d95070d18be5df279b6e9c4f886b7f
Matt Cano <cano.matt@gmail.com>
2013-04-12 08:16:04 -0700

    barely functioning cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :update_cart
+
+  def update_cart
+    @cart_products = Product.where(:in_cart => true)
+    @cart_total = 0
+    @cart_products.each{|p| @cart_total += p.price}
+  end
 end
